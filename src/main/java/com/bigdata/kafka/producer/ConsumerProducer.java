18c431f96adf5da53b37cb78c8e0d58783a1360d
jone <jone@163.com>
2020-06-10 14:47:56 +0800

    kafka

--- src/main/java/com/bigdata/kafka/producer/ConsumerProducer.java
+++ src/main/java/com/bigdata/kafka/producer/ConsumerProducer.java
@@ -2,7 +2,6 @@ package com.bigdata.kafka.producer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-
 import java.util.Properties;
 
 public class ConsumerProducer {
