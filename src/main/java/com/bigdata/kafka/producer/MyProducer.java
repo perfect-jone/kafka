740382d3926cf1c43ec2e46c74aa229ef7d1d8be
jone <jone@163.com>
2020-06-11 09:47:04 +0800

    kafka

--- src/main/java/com/bigdata/kafka/producer/MyProducer.java
+++ src/main/java/com/bigdata/kafka/producer/MyProducer.java
@@ -1,9 +1,7 @@
 package com.bigdata.kafka.producer;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
+
 import java.util.Properties;
 
 public class MyProducer {
@@ -15,20 +13,27 @@ public class MyProducer {
         props.put("bootstrap.servers", "hadoop101:9092,hadoop102:9092,hadoop103:9092");
 
         // ACK应答机制，取值有0,1,all,效率和安全性
+        // props.put(ProducerConfig.ACKS_CONFIG,"all");
         props.put("acks", "all");
 
         // 生产者向kafka发送消息出现错误时的重试次数
+        // props.put(ProducerConfig.RETRIES_CONFIG, 0);
         props.put("retries", 0);
 
         // 批处理大小和发送延迟，每个Batch要存放16k数据或者1ms后才可以发到Broker上去
+        // props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        // props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
         props.put("batch.size", 16384);
         props.put("linger.ms", 1);
 
         // 内存缓冲区大小，通过KafkaProducer发送出去的消息先进入到客户端本地的内存缓冲区，然后把很多消息收集成一个一个的Batch，再发送到Broker上去的
-        // 如过Kafka发送消息的速度 > 消息发送到Broker的速度,那么消息就会在缓冲区堆积，导致缓冲区不足
+        // 如过Kafka发送消息到缓冲区的速度 > 消息发送到Broker的速度,那么消息就会在缓冲区堆积，导致缓冲区不足
+        // props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
         props.put("buffer.memory", 33554432);
 
         // kv序列化类
+        // props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        // props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
